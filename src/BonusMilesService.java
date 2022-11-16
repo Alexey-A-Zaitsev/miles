deebbca8e6b42ede27804daa7fb7b534a7dd6450
Alex Zaitsev <saizewalex@gmail.com>
2022-11-16 20:42:44 +0300

    BonusMilesService class added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+
+        int result = cost / 20;
+        return result;
+
+            }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int cost = 21_367; // Стоимость билета
-        int mile = 20; // Количество рублей за 1 милю
-        int bonus = cost / mile; // Количество бонусных миль
-
-        System.out.println("Бонусные мили: " + bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
