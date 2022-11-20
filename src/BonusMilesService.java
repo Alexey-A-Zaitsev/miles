e61fe861d445d0dfa9ec96b7cf9707dab7375849
Alex Zaitsev <saizewalex@gmail.com>
2022-11-20 21:59:45 +0300

    Reformat code 2

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,6 @@
 public class BonusMilesService {
     public int calculate(int cost) {
+
         int result = cost / 20;
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
+
         System.out.println(miles);
     }
 }
